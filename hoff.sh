da65affb869eadc8c2418dd87eed1ac40a4f7719
Helge van Scherpenzeel <helge@moddix.com>
2019-01-31 14:39:06 +0100

    updated image location

--- hoff.sh
+++ hoff.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # download a fancy hasselhof
-curl -o /tmp/david.jpeg http://www.walesoncraic.com/wp-content/uploads/2014/11/david-hasselhof.jpeg
+curl -o /tmp/david.jpeg http://moddix.com/hoff.jpeg
 
 if [ `uname` == "Darwin" ]; then
         osascript -e 'tell application "System Events" to tell every desktop to set picture to "/tmp/david.jpeg"'
